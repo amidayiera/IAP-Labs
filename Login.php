827c26022eb1322b9450938bb00518f3a2a8b666
Nasha <amidayiera@gmail.com>
2020-05-27 23:35:26 +0300

    add Lab1.css file | include Connection.php once

--- Login.php
+++ Login.php
@@ -1,21 +1,28 @@
 <?php
-    include_once 'DBConnector.php';
+    include_once 'Connection.php';
     include_once 'User.php';
 
     $connection = new DBconnector;
     if(isset($_POST['btn_login'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
+
         $instance = User::create();
         $instance->setPassword($password);
         $instance->setUsername($username);
 
         if($instance->isPasswordCorrect()) {
             $instance->login();
+            // close database connection
             $connection->closeConnection();
+            // create a user session
             $instance->createUserSession();
+            header("Location:PrivatePage.php");
 
-        } else {
+        // if(User::isPasswordCorrect($username, $password)) {
+        //     User::createUserSession($username);
+        //     header("Location:PrivatePage.php");
+        // } else {
             $connection->closeConnection();
             header("Location:Login.php");
         }
@@ -31,7 +38,8 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="css/Lab1.css">
         <script src="js/validate.js" type="text/javascript"></script>
-        <link rel="stylesheet" href="css/validate.css">
+        <link rel="stylesheet" href="css/Validate.css">
+        <link rel="stylesheet" href="css/Lab1.css">
     </head>
     <body>
         <!-- PHP SELF means submitting this form to itself for processing -->
